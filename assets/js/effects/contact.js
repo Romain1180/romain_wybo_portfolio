f75c3b2ad451f912bd002af2ee06076f0e1db5b5
Romain1180 <romain.wybo1@gmail.com>
2023-11-17 10:32:27 +0100

    add mailto in contact section

--- assets/js/effects/contact.js
+++ assets/js/effects/contact.js
@@ -1,15 +1,16 @@
 let contact1 = document.querySelector('.contact-1');
 let contact2 = document.querySelector('.contact-2');
 let contact3 = document.querySelector('.contact-3 ');
+let contactLink1 = document.querySelector('.contact-1 a');
 let contactLink2 = document.querySelector('.contact-2 a');
 let contactLink3 = document.querySelector('.contact-3 a');
 
 contact1.addEventListener('mouseover', () => {
-  contact1.style.color = '#010101';
+  contactLink1.style.color = '#010101';
   // cursorLink.style.backgroundColor = '#010101';
 })
 contact1.addEventListener('mouseout', () => {
-  contact1.style.color = '#C6C6C6';
+  contactLink1.style.color = '#C6C6C6';
   // cursorLink.style.backgroundColor = '#C6C6C6';
 })
 
